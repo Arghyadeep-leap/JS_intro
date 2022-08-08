08e05c79d65da7156563aa5feb48b04508f89404
Arghyadeep-Leap <107385481+Arghyadeep-leap@users.noreply.github.com>
2022-08-08 18:44:55 +0530

    Add files via upload

---
+++ 8.js
@@ -0,0 +1,21 @@
+var x = 10;
+var y = 22;
+
+var a = (x < 9 && y < 9) 
+var b = (x > 9 && y > 9)
+console.log(a, b);
+
+
+
+// check if the number is positive, negative or zero
+const number = 36
+
+if (number >= 0) {
+    if (number == 0) {
+        console.log("You entered number 0");
+    } else {
+        console.log("You entered a positive number");
+    }
+} else {
+    console.log("You entered a negative number");
+}

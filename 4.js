f8faf0e32fcc9a07b19a4d885232c1da9905e1ce
Arghyadeep-Leap <107385481+Arghyadeep-leap@users.noreply.github.com>
2022-08-08 15:24:16 +0530

    Learned about JS Operators

---
+++ 4.js
@@ -0,0 +1,16 @@
+// JS Operator
+
+let x =4;
+let y =5;
+
+console.log(x + y) // addition
+
+console.log(x -y) // subtraction
+
+console.log(x * y) // multiplication
+
+console.log(y % x) //modulus 
+
+console.log(y / x) // division
+
+console.log(x ** y) // power
---
+++ 5.js
@@ -0,0 +1,5 @@
+// increment operator
+
+var x = 3;
+console.log(x++);// preincrement
+console.log(++x);// post increment
